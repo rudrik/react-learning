5819db1fae50baaf2b9035fcccff12dc906eec1e
rudrik.patel@zymr.com <rudrik.patel@zymr.com>
2020-11-12 14:52:07 +0530

    ExpensifyApp -- Added production source map build. -- setup webpack config for the production.

--- expensify-app/src/components/Header.js
+++ expensify-app/src/components/Header.js
@@ -5,8 +5,8 @@ const Header = () => (
     <header>
         <h1>Expensify</h1>
         <NavLink to="/" activeClassName="is-active" exact={true}>DashBoard</NavLink>
-        <NavLink to="/create" activeClassName="is-active">Add Expense</NavLink>
-        <NavLink to="/help" activeClassName="is-active">Help</NavLink>
+        <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
+        {/* <NavLink to="/help" activeClassName="is-active">Help</NavLink> */}
     </header>
 );
 
